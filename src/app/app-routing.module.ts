f6f4634c521c7d20636f579c5b4a3c8dbab819ae
Jeanpier Mendoza <jeanpi3rm@gmail.com>
2022-03-06 20:17:34 -0500

    remove unsued imports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './ppg/pages/dashboard/dashboard.component';
-import { LoginComponent } from './auth/pages/login/login.component';
-import { RegisterComponent } from './auth/pages/register/register.component';
-import { ProfilesComponent } from './ppg/pages/profiles/profiles.component';
-import { GenerateComponent } from './ppg/pages/generate/generate.component';
-import { AccountComponent } from './ppg/pages/account/account.component';
-import { RequireAuthGuard } from './auth/guards/require-auth.guard';
 import { LoginGuard } from './auth/guards/login.guard';
+import { RequireAuthGuard } from './auth/guards/require-auth.guard';
 import { PpgComponent } from './ppg/ppg.component';
 
 const routes: Routes = [
