8441e962ffac337311c9b235584a0e560adbe377
Jeanpier Mendoza <jeanpi3rm@gmail.com>
2022-06-19 17:28:54 -0500

    feat: set authguard on profile routes

--- src/app/main/profiles/profiles-routing.module.ts
+++ src/app/main/profiles/profiles-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from '../auth/guards/auth.guard';
 import { DiscoverComponent } from './pages/discover/discover.component';
 import { ProfileListComponent } from './pages/profile-list/profile-list.component';
 import { ProfileComponent } from './pages/profile/profile.component';
@@ -9,7 +10,10 @@ const routes: Routes = [
     path: '',
     component: ProfileListComponent,
     pathMatch: 'full',
+    canLoad: [AuthGuard],
+    canActivate: [AuthGuard],
   },
+
   {
     path: 'descubrir',
     component: DiscoverComponent,
@@ -17,6 +21,8 @@ const routes: Routes = [
   {
     path: ':id',
     component: ProfileComponent,
+    canLoad: [AuthGuard],
+    canActivate: [AuthGuard],
   },
 ];
 
