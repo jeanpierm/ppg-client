45e487a38f808813f27fae4217b18109ee63cbd8
Jeanpier Mendoza <jeanpi3rm@gmail.com>
2022-07-18 16:19:37 -0500

    refactor: status options as array in .ts file

--- src/app/admin/pages/tech-types/tech-types.component.ts
+++ src/app/admin/pages/tech-types/tech-types.component.ts
@@ -10,6 +10,7 @@ import { MatPaginator } from '@angular/material/paginator';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { debounceTime, distinctUntilChanged, fromEvent } from 'rxjs';
 import { AlertService } from 'src/app/core/services/alert.service';
+import { EntityStatus } from '../../../core/enums/entity-status.enum';
 import { TechTypeDialogComponent } from '../../components/tech-type-dialog/tech-type-dialog.component';
 import { TechType } from '../../interfaces/tech-type.interface';
 import { TechTypesService } from '../../services/tech-types.service';
@@ -23,7 +24,11 @@ export class TechTypesComponent implements OnInit, AfterViewInit {
   static readonly PATH = 'tech-types';
   readonly defaultPageSize = 10;
   displayedColumns: string[] = ['Label', 'Nombre', 'Acciones'];
-  status = 'A';
+  status = EntityStatus.Active;
+  statusOptions = [
+    { label: 'Activos', value: EntityStatus.Active },
+    { label: 'Eliminados', value: EntityStatus.Inactive },
+  ];
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild('input') input!: ElementRef;
@@ -88,7 +93,7 @@ export class TechTypesComponent implements OnInit, AfterViewInit {
           ? this.techTypesService.updateTechType(type.techTypeId, type)
           : this.techTypesService.saveTechType(type);
         action.subscribe({
-          next: (res) => {
+          next: () => {
             this.loadTechTypePages();
             this.alertService.success(
               'Tipo de tecnología guardado exitosamente!'
@@ -109,7 +114,7 @@ export class TechTypesComponent implements OnInit, AfterViewInit {
       .then((resp) => {
         if (resp.isConfirmed) {
           this.techTypesService.deleteTechType(type.techTypeId).subscribe({
-            next: (res) => {
+            next: () => {
               this.loadTechTypePages();
               this.alertService.success(
                 'Tipo de tecnología eliminado exitosamente!'
---
+++ src/app/core/enums/entity-status.enum.ts
@@ -0,0 +1,4 @@
+export enum EntityStatus {
+  Active = 'A',
+  Inactive = 'I',
+}
